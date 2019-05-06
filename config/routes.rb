2891704e6679531910087178183437071de86d58
unknown <dusandjordjevic.work@gmail.com>
2019-05-06 14:27:11 +0200

    Basic functions

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :tweets
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "tweets#index"
+end
